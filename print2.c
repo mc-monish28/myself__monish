cb286357d2a6ff5383bb2db45f58198629d6aad5
mc-monish28 <116776241+mc-monish28@users.noreply.github.com>
2023-01-14 21:18:36 +0530

    Add files via upload

---
+++ print2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+printf("%10s","hello");
+return 0;
+       }
---
+++ redefining error.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int var=2;
+    int var=3;
+    printf("%d\n",var);
+    printf("%d\n",var);
+    return 0;
+}
